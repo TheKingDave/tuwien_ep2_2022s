3d9d1462aafd9120ec73cfe94c8bbb2edb4ac4f0
TheKingDave <david@langheiter.com>
2022-03-23 20:32:30 +0100

    Added sol simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,6 +1,7 @@
 import codedraw.CodeDraw;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Random;
 
 // 1. Grouping of methods and variables together to form units. Example: the new Body/Vector3
@@ -43,17 +44,31 @@ public class Simulation {
         
         BodyQueue bodies = new BodyQueue(NUMBER_OF_BODIES);
         BodyForceMap forceOnBody = new BodyForceMap(NUMBER_OF_BODIES);
+        
+        // Call program with argument "sol" to run the solar system simulation
+        if (args.length > 0 && args[0].equalsIgnoreCase("sol")) {
+            bodies.add(new Body(1.989e30,new Vector3(0,0,0),new Vector3(0,0,0)));
+            bodies.add(new Body(5.972e24,new Vector3(-1.394555e11,5.103346e10,0),new Vector3(-10308.53,-28169.38,0)));
+            bodies.add(new Body(3.301e23,new Vector3(-5.439054e10,9.394878e9,0),new Vector3(-17117.83,-46297.48,-1925.57)));
+            bodies.add(new Body(4.86747e24,new Vector3(-1.707667e10,1.066132e11,2.450232e9),new Vector3(-34446.02,-5567.47,2181.10)));
+            bodies.add(new Body(6.41712e23,new Vector3(-1.010178e11,-2.043939e11,-1.591727E9),new Vector3(20651.98,-10186.67,-2302.79)));
+            for(int i = 0; i < bodies.size(); i++) {
+                Body b = bodies.poll();
+                forceOnBody.put(b, new Vector3());
+                bodies.add(b);
+            }
+        } else {
+            Random random = new Random(2022);
 
-        Random random = new Random(2022);
-
-        for (int i = 0; i < NUMBER_OF_BODIES; i++) {
-            Body b = new Body(
-                    Math.abs(random.nextGaussian()) * OVERALL_SYSTEM_MASS / NUMBER_OF_BODIES,
-                    new Vector3(0.2 * random.nextGaussian() * AU, 0.2 * random.nextGaussian() * AU, 0.2 * random.nextGaussian() * AU),
-                    new Vector3(0 + random.nextGaussian() * 5e3, 0 + random.nextGaussian() * 5e3, 0 + random.nextGaussian() * 5e3)
-            );
-            forceOnBody.put(b, new Vector3());
-            bodies.add(b);
+            for (int i = 0; i < NUMBER_OF_BODIES; i++) {
+                Body b = new Body(
+                        Math.abs(random.nextGaussian()) * OVERALL_SYSTEM_MASS / NUMBER_OF_BODIES,
+                        new Vector3(0.2 * random.nextGaussian() * AU, 0.2 * random.nextGaussian() * AU, 0.2 * random.nextGaussian() * AU),
+                        new Vector3(0 + random.nextGaussian() * 5e3, 0 + random.nextGaussian() * 5e3, 0 + random.nextGaussian() * 5e3)
+                );
+                forceOnBody.put(b, new Vector3());
+                bodies.add(b);
+            }
         }
 
         double seconds = 0;
