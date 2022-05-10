608b71d41317210dfeba119672fd92449969c0e0
David LANGHEITER (EP2 participant) <p12123685@inf139.inflab.tuwien.ac.at>
2022-05-10 18:26:30 +0200

    ue5-init

--- src/NamedBody.java
+++ src/NamedBody.java
@@ -1,42 +1,70 @@
-public class NamedBody /* TODO: add clause(s) */
-{
+import codedraw.CodeDraw;
+
+import java.util.Objects;
+
+public class NamedBody implements Massive {
 
     // TODO: add missing parts of this class.
+    private String name;
+    private double mass;
+    private Vector3 massCenter;
+    private Vector3 currentMovement;
 
     // Initializes this with name, mass, current position and movement. The associated force
     // is initialized with a zero vector.
     public NamedBody(String name, double mass, Vector3 massCenter, Vector3 currentMovement) {
-        // TODO: implement constructor.
-
+        this.name = name;
+        this.mass = mass;
+        this.massCenter = massCenter;
+        this.currentMovement = currentMovement;
     }
 
     // Returns the name of the body.
     public String getName() {
-        // TODO: implement method.
-        return "";
-
+        return name;
     }
 
     // Compares `this` with the specified object. Returns `true` if the specified `o` is not
     // `null` and is of type `NamedBody` and both `this` and `o` have equal names.
     // Otherwise `false` is returned.
     public boolean equals(Object o) {
-        //TODO: implement method.
-        return false;
-
+        return o instanceof NamedBody && (o == this || ((NamedBody)o).name.equals(this.name));
     }
 
     // Returns the hashCode of `this`.
+    @Override
     public int hashCode() {
-        //TODO: implement method.
-        return 0;
-
+        return Objects.hash(name, mass, massCenter, currentMovement);
     }
 
     // Returns a readable representation including the name of this body.
     public String toString() {
-        //TODO: implement method.
-        return "";
+        return this.name;
+    }
 
+
+    @Override
+    public void draw(CodeDraw cd) {
+        cd.setColor(SpaceDraw.massToColor(mass));
+        this.massCenter.drawAsFilledCircle(cd, radius());
+    }
+
+    @Override
+    public double getMass() {
+        return this.mass;
+    }
+
+    @Override
+    public Vector3 getMassCenter() {
+        return this.massCenter;
+    }
+
+    @Override
+    public void move(Vector3 force) {
+        Vector3 newPos = force.times(1 / mass).plus(massCenter).plus(currentMovement);
+        Vector3 newMov = newPos.minus(massCenter);
+
+        massCenter = newPos;
+        currentMovement = newMov;
     }
 }
